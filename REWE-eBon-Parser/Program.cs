bba46233a612363285623b9267c530425e4261d2
Daniel Kirstenpfad <bietiekay@gmail.com>
2022-09-03 11:52:38 +0200

    changing back to the old style... i am old.

--- REWE-eBon-Parser/Program.cs
+++ REWE-eBon-Parser/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace REWEeBonParser // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
